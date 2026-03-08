223024b9425d46341174e592847c3cb41504b743
ahmetefeyaman <ahmetefeyaman5@gmail.com>
2026-03-09 00:46:40 +0300

    Create satis_sayisi.cpp

---
+++ basics/satis_sayisi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int su, sandvic, total = 0;
+    cout << "su ve sandvic sayisini giriniz." << endl;
+    cin >> su >> sandvic;
+    total = su + sandvic;
+    cout << "toplam satis = " << total << endl;
+    return 0;
+}
