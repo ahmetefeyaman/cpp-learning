2e6673022cd5d522ccfd9f840421a8f3d749dde7
ahmetefeyaman <ahmetefeyaman5@gmail.com>
2026-03-10 23:56:20 +0300

    Create x.cpp

---
+++ basics/x.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+   cout<<"gun sayisi gir."<<endl;
+    cin>>n;
+     if(n <= 0)
+      cout<<"Invalid";
+     else
+      cout<<"OK"; 
+       return 0;
+}
