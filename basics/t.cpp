b7cd4b5073df8710cca2603cb72e2b8ed7165bec
ahmetefeyaman <ahmetefeyaman5@gmail.com>
2026-03-10 23:57:54 +0300

    Create t.cpp

---
+++ basics/t.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int k,puan;
+   cout<<"giris sayisini giriniz"<<endl;
+    cin>> k;
+     puan = 2*k;
+      cout<<"puaninin: "<<puan<<endl;
+
+  return 0;
+}
