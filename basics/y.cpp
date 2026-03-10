97887b9f8193ce29369447ead033564f48f4b072
ahmetefeyaman <ahmetefeyaman5@gmail.com>
2026-03-10 23:56:53 +0300

    Create y.cpp

---
+++ basics/y.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x,y,z;
+  double ort;
+   cout<<"3 degeri giriniz."<<endl;
+    cin>>x>>y>>z;
+     ort= (x+y+z)/3;
+      cout<<"Rapor : "<<ort<<endl;
+
+       return 0;
+}
