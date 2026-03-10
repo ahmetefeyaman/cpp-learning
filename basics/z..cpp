1cd2db1dcceb6634e223f747cfa9073e894c9f0d
ahmetefeyaman <ahmetefeyaman5@gmail.com>
2026-03-10 23:57:21 +0300

    Create z..cpp

---
+++ basics/z..cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b,sum,diff;
+   cout << "degerleri giriniz." << endl;
+    cin >> a >> b;
+     sum= a+b;
+     diff= a-b;
+      cout << "sum : " << sum << " diff : " << diff <<endl;
+  return 0;
+}
